12676804335561643b02a981e344a99d477a1171
Ali Rostami <alirostami.1313@gmail.com>
2021-07-25 17:57:50 +0430

    added some boilerplate code

--- cmd/baagh/main.go
+++ cmd/baagh/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"time"
 
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+func main() {
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer rpio.Close()
+
+	pin = rpio.Pin(10)
+	pin.Output()
+
+	for x := 0; x < 20; x++ {
+		pin.Toggle()
+		time.Sleep(time.Second)
+	}
 }
