31ca940e2181202ef37facfeef4d7cfd775f170b
Ali Rostami <alirostami.1313@gmail.com>
2021-07-26 11:34:18 +0430

    added viper(not really needed right now but I WANTED TO), and fixed logger

--- cmd/baagh/main.go
+++ cmd/baagh/main.go
@@ -4,23 +4,38 @@ import (
 	"log"
 	"time"
 
+	"github.com/spf13/viper"
 	"github.com/stianeikeland/go-rpio/v4"
 	"go.uber.org/zap"
 )
 
 func main() {
-	// build zap logger
-	zap.NewDevelopment()
-	logger, err := zap.NewProduction()
+	// viper stuff
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("/etc/baagh/")
+	viper.AddConfigPath("$HOME/.baagh")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error
+		} else {
+			log.Fatal(err)
+		}
+	}
 
+	// build zap logger
+	logger, err := zap.NewProduction()
 	if err != nil {
 		log.Fatalf("can't initialize zap logger: %v", err)
 	}
 	defer logger.Sync()
 
+	sugar := logger.Sugar()
+
 	// initialize rpio package and allocate memory
 	if err := rpio.Open(); err != nil {
-		log.Fatalf("can't open and memory map GPIO memory range from /dev/mem: %v", err)
+		sugar.Fatalf("can't open and memory map GPIO memory range from /dev/mem: %v", err)
 	}
 	defer rpio.Close()
 
--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/AliRostami1/baagh
 go 1.16
 
 require (
+	github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6 // indirect
+	github.com/coreos/etcd v3.3.10+incompatible // indirect
+	github.com/coreos/go-etcd v2.0.0+incompatible // indirect
+	github.com/spf13/viper v1.8.1
 	github.com/stianeikeland/go-rpio/v4 v4.4.0
+	github.com/ugorji/go/codec v0.0.0-20181204163529-d75b2dcb6bc8 // indirect
+	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
 	go.uber.org/zap v1.18.1
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
 	golang.org/x/tools v0.1.2 // indirect
