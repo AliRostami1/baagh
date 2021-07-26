43e3a4d796bc9a44fee86e5d48b6028ac14277db
Ali Rostami <alirostami.1313@gmail.com>
2021-07-26 23:11:55 +0430

    added sensor package

---
+++ pkg/sensor/sensor.go
@@ -0,0 +1,26 @@
+package gopio
+
+import "github.com/stianeikeland/go-rpio/v4"
+
+type SensorCallback = func(current rpio.State)
+
+func SensorFn(pin int, fn SensorCallback) {
+	ch := make(chan rpio.State)
+	go Sensor(pin, ch)
+	for state := range ch {
+		fn(state)
+	}
+}
+
+func Sensor(pin int, ch chan<- rpio.State) {
+	p := rpio.Pin(pin)
+	p.Input()
+	prevState := rpio.State(0)
+	for {
+		state := p.Read()
+		if state != prevState {
+			prevState = state
+			ch <- state
+		}
+	}
+}
