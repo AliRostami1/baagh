b96a2c9fee0b95da4bfcb15e1cbbf9906a8d29d7
Ali Rostami <ali.rostami@live.com>
2021-08-01 22:53:56 +0430

    added badger database to code

---
+++ pkg/database/database.go
@@ -0,0 +1,85 @@
+package database
+
+import (
+	"context"
+
+	"github.com/dgraph-io/badger/v3"
+)
+
+type EventListener func(key string, value string)
+
+type DB struct {
+	*badger.DB
+	eventListeners map[string][]EventListener
+}
+
+type Options struct {
+	path   string
+	logger badger.Logger
+}
+
+// TODO add our custom logger to this
+func New(ctx context.Context, opt *Options) (*DB, error) {
+	db, err := badger.Open(badger.DefaultOptions(opt.path).WithLogger(opt.logger))
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		defer db.Close()
+		<-ctx.Done()
+	}()
+	return &DB{
+		DB:             db,
+		eventListeners: make(map[string][]EventListener),
+	}, nil
+}
+
+func (d *DB) Get(key string) (string, error) {
+	var value []byte
+	err := d.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(key))
+		if err != nil {
+			return err
+		}
+		item.Value(func(val []byte) error {
+			copy(value, val)
+			return nil
+		})
+		return err
+	})
+	return string(value), err
+}
+
+func (d *DB) Set(key string, value string) error {
+	isDifferent := false
+	err := d.Update(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(key))
+		if err != nil {
+			return err
+		}
+		item.Value(func(val []byte) error {
+			if string(val) != value {
+				isDifferent = true
+			}
+			return nil
+		})
+		if isDifferent {
+			err = txn.Set([]byte(key), []byte(value))
+		}
+		return err
+	})
+	if isDifferent {
+		d.executeEvents(key, value)
+	}
+	return err
+}
+
+func (d *DB) On(key string, fn ...EventListener) {
+	d.eventListeners[key] = append(d.eventListeners[key], fn...)
+}
+
+func (d *DB) executeEvents(key string, value string) {
+	for _, fn := range d.eventListeners[key] {
+		fn(key, value)
+	}
+}
