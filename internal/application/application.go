9c43f6268d590e30d223350285735e73a53b2849
Ali Rostami <ali.rostami@live.com>
2021-08-08 12:36:45 +0430

    removed unused paths

--- internal/application/application.go
+++ internal/application/application.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 	"path/filepath"
 
 	"github.com/AliRostami1/baagh/pkg/config"
@@ -20,18 +19,11 @@ type Application struct {
 	DB       *database.DB
 	Gpio     *gpio.Gpio
 	Ctx      context.Context
-	Path     string
 	Shutdown func(string)
 	Cleanup  func() error
 }
 
 func New() (*Application, error) {
-	userHomeDir, err := os.UserHomeDir()
-	if err != nil {
-		return nil, err
-	}
-	path := userHomeDir + "/.baagh/"
-
 	// this is the application context, it will determine when the application will exit
 	ctx, cancelCtx := context.WithCancel(context.Background())
 
@@ -51,7 +43,7 @@ func New() (*Application, error) {
 	config, err := config.New(&config.ConfigOptions{
 		ConfigName:  "config",
 		ConfigType:  "yaml",
-		ConfigPaths: []string{"/etc/baagh/", "$HOME/.baagh", "."},
+		ConfigPaths: []string{"/etc/baagh/"},
 	})
 	// we temporarily ignore this check so it doesn't terminate the program, untill we add config support
 	if err != nil {
@@ -63,7 +55,7 @@ func New() (*Application, error) {
 
 	// Connect to and Initialize a db instnace
 	db, err := database.New(ctx, &database.Options{
-		Path:   filepath.Join(path, "badger"),
+		Path:   filepath.Join("/var/log/baagh/badger"),
 		Logger: logger,
 	})
 	if err != nil {
@@ -97,7 +89,6 @@ func New() (*Application, error) {
 		DB:       db,
 		Gpio:     gpio,
 		Ctx:      ctx,
-		Path:     path,
 		Shutdown: shutdown,
 		Cleanup:  cleanup,
 	}, nil
